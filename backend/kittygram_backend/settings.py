c991c5de2c4d6198a4600ae1d9bcef7abcc0a25d
Ксения Заболоцкая <kseniyazabolotzkaya@gmail.com>
2023-12-10 20:43:49 +0300

    Исправляю деплой 4

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY', '1')
 
 DEBUG = True
 
@@ -99,6 +99,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
